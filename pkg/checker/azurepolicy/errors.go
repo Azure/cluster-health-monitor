1100e9041995d49096ec4c3461d20066898febff
carlos <carlosalv@microsoft.com>
2025-08-06 11:22:35 -0700

    update error

--- pkg/checker/azurepolicy/errors.go
+++ pkg/checker/azurepolicy/errors.go
@@ -2,6 +2,6 @@ package azurepolicy
 
 const (
 	// This is the error code of the AzurePolicyChecker's result.
-	errCodeAzurePolicyTimeout     = "azure_policy_dry_run_timeout"
+	errCodeAzurePolicyTimeout     = "azure_policy_timeout"
 	errCodeAzurePolicyNoViolation = "azure_policy_no_violation"
 )
