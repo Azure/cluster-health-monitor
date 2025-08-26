ae08fbde8f10728e8e48446a2f5f4e200abf4285
carlos <carlosalv@microsoft.com>
2025-08-26 14:22:43 -0700

    remove unsused error code

--- pkg/checker/azurepolicy/errors.go
+++ pkg/checker/azurepolicy/errors.go
@@ -2,6 +2,5 @@ package azurepolicy
 
 const (
 	// This is the error code of the AzurePolicyChecker's result.
-	errCodeRequestTimeout                = "request_timeout"
 	errCodeAzurePolicyEnforcementMissing = "azure_policy_enforcement_missing"
 )
