41ca4dd2c73a8d6286aef55b5c985d77025ae7b9
Mitch Shao <minsha@microsoft.com>
2025-11-25 12:01:38 -0800

    rename poknetwork -> podnetwork

--- pkg/checker/poknetwork/errors.go
+++
@@ -1,8 +0,0 @@
-package podnetwork
-
-// Error codes for PodNetwork checker results
-const (
-	// ErrorCodeNetworkConnectivityFailed indicates pod network connectivity issues
-	// This covers both pod-to-pod connectivity failures and cluster DNS service failures
-	ErrorCodeNetworkConnectivityFailed = "NetworkConnectivityFailed"
-)
