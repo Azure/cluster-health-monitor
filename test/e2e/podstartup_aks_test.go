088c62d8ae3f51453fd531b00050f78712398e2c
carlos <carlosalv@microsoft.com>
2025-11-26 15:31:47 -0800

    split csi and nap into two checkers

--- test/e2e/podstartup_aks_test.go
+++ test/e2e/podstartup_aks_test.go
@@ -8,11 +8,12 @@ import (
 )
 
 var (
-	// Note that podStartupWithNAPAndCSICheckerName must match with the configmap in manifests/overlays/test-aks.
-	podStartupWithNAPAndCSICheckerNames = []string{"TestPodStartupWithNAPAndCSI"}
+	// Note that these checker names must match with the configmap in manifests/overlays/test-aks.
+	podStartupWithNAPCheckerNames = []string{"TestPodStartupWithNAP"}
+	podStartupWithCSICheckerNames = []string{"TestPodStartupWithCSI"}
 )
 
-var _ = Describe("Pod startup checker with NAP and CSI", Ordered, ContinueOnFailure, func() {
+var _ = Describe("Pod startup checker with NAP", Ordered, ContinueOnFailure, func() {
 	var (
 		session   *gexec.Session
 		localPort int
@@ -28,11 +29,36 @@ var _ = Describe("Pod startup checker with NAP and CSI", Ordered, ContinueOnFail
 		safeSessionKill(session)
 	})
 
-	It("should report healthy status for pod startup checker with NAP and CSI", func() {
-		By("Waiting for pod startup checker with NAP and CSI metrics to report healthy status")
+	It("should report healthy status for pod startup checker with NAP", func() {
+		By("Waiting for pod startup checker with NAP metrics to report healthy status")
 		waitForCheckerResultsMetricsValueIncrease(localPort,
-			checkerResultMetricName, podStartupWithNAPAndCSICheckerNames, checkerTypePodStartup, metricsHealthyStatus, metricsHealthyErrorCode,
+			checkerResultMetricName, podStartupWithNAPCheckerNames, checkerTypePodStartup, metricsHealthyStatus, metricsHealthyErrorCode,
 			13*time.Minute, 10*time.Second,
-			"Pod startup checker with NAP and CSI metrics did not report healthy status within the timeout period")
+			"Pod startup checker with NAP metrics did not report healthy status within the timeout period")
+	})
+})
+
+var _ = Describe("Pod startup checker with CSI", Ordered, ContinueOnFailure, func() {
+	var (
+		session   *gexec.Session
+		localPort int
+	)
+
+	BeforeEach(func() {
+		By("Ensuring required node labels exist for scheduling synthetic pods created by pod startup checker")
+		ensureLabelsExistOnAtLeastOneNode(clientset, requiredNodeLabelsForSchedulingSyntheticPods)
+		session, localPort = setupMetricsPortforwarding(clientset)
+	})
+
+	AfterEach(func() {
+		safeSessionKill(session)
+	})
+
+	It("should report healthy status for pod startup checker with CSI", func() {
+		By("Waiting for pod startup checker with CSI metrics to report healthy status")
+		waitForCheckerResultsMetricsValueIncrease(localPort,
+			checkerResultMetricName, podStartupWithCSICheckerNames, checkerTypePodStartup, metricsHealthyStatus, metricsHealthyErrorCode,
+			7*time.Minute, 10*time.Second,
+			"Pod startup checker with CSI metrics did not report healthy status within the timeout period")
 	})
 })
