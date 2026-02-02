a830be8d865c081a148f6fb0ecc4ab05f1d84459
Mitch Shao <minsha@microsoft.com>
2026-02-02 22:48:34 +0000

    add the crd files

---
+++ apis/upgradenodeinprogresses/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// Package v1alpha1 contains API Schema definitions for the upgradenodeinprogresses v1alpha1 API group
+// +kubebuilder:object:generate=true
+// +groupName=health.aks.io
+
+package v1alpha1
